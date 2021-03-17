6bba3f5c2d928b665163e5f261e9720dfec7b912
Randhir <randhir4444@gmail.com>
2021-03-17 23:57:00 +0530

    done

--- src/TempApp.js
+++ src/TempApp.js
@@ -1,23 +1,24 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
 function TempApp() {
     const [city, setcity] = useState(null);
     const [search, setsearch] = useState("mumbai");
 
-    const Data=(event)=>{
-        const name1=event.target.value;
+    const Data = (event) => {
+        const name1 = event.target.value;
         setsearch(name1);
     }
     useEffect(() => {
-            const fetchData = async () => {
-                const url=`http://api.openweathermap.org/data/2.5/weather?q=${search}&appid=1f6d8fae692fb631bf07a87cbda89596`;
-                const response = await fetch(url);
-                const resJson=await response.json();
-                console.log(resJson);
-                setcity(resJson.main)
-            }
-            fetchData()  
-    },[search]);
+        const fetchData = async () => {
+            const url = `http://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=1f6d8fae692fb631bf07a87cbda89596`;
+            const response = await fetch(url);
+            const resJson = await response.json();
+            console.log(resJson);
+            console.log(resJson);
+            setcity(resJson.main)
+        }
+        fetchData()
+    }, [search]);
     // useEffect(() => {
     //     async function getData(){
     //         const res=await axios.get(`api.openweathermap.org/data/2.5/weather?q=${city}&appid=1f6d8fae692fb631bf07a87cbda89596`);
@@ -29,27 +30,27 @@ function TempApp() {
         <>  <div className="check">
             <div className="main">
                 <div className="input">
-                    <label>Enter cityName</label>
-                    <input type="search" onChange={Data} className="form-control" aria-describedby="emailHelp" placeholder="Patna" />
+                    <label className="head">Enter cityName</label>
+                    <input type="search" onChange={Data} className="form" value={search} placeholder="Patna" />
                 </div>
                 {
-                    !city?(<p>No Data Found</p>):(
-                        
-                    <div>
-                    <div className="data">
-                    <h2><i className="fas fa-street-view"></i> {search}</h2>
-                    <h1 className="temp">
-                        {city.temp}
-                    </h1>
-                    <h3 className="min_max">
-                        min:5.16 max:5.36
-                    </h3>
-                </div>
-                    </div>)
+                    !city ? (<p>No Data Found</p>) : (
+
+                        <div>
+                            <div className="data">
+                                <h2><i className="fas fa-street-view"></i> {search}</h2>
+                                <h1 className="temp">
+                                    {city.temp}
+                                </h1>
+                                <h3 className="min_max">main.temp_max
+                        min:{city.temp_min} max:{city.temp_min}
+                                </h3>
+                            </div>
+                        </div>)
                 }
-                
+
             </div>
-            </div>
+        </div>
         </>
     )
 }
